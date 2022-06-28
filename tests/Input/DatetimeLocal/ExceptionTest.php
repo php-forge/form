7589f962c7191a48ea7fe5692d8a310dab836cf9
Wilmer Arambula <terabytesoftw@gmail.com>
2022-06-28 15:36:56 -0400

    Corregir pruebas de mutacion.

--- tests/Input/DatetimeLocal/ExceptionTest.php
+++ tests/Input/DatetimeLocal/ExceptionTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Forge\Form\Tests\Input\DateTimeLocal;
 
-use Forge\Form\Input\DateTimeLocal;
+use Forge\Form\Input\DatetimeLocal;
 use Forge\Form\Tests\Support\PropertyTypeForm;
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
