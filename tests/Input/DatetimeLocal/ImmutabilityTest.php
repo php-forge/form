af44fd0b7d325a5ff70c284ae7a7fc445205828f
Wilmer Arambula <terabytesoftw@gmail.com>
2022-06-29 16:55:54 -0400

    Arreglar error tipografico.

--- tests/Input/DatetimeLocal/ImmutabilityTest.php
+++ tests/Input/DatetimeLocal/ImmutabilityTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Forge\Form\Tests\Input\DatetimeLocal;
 
-use Forge\Form\Input\DateTimeLocal;
+use Forge\Form\Input\DatetimeLocal;
 use Forge\Form\Tests\Support\PropertyTypeForm;
 use PHPUnit\Framework\TestCase;
 use ReflectionException;
@@ -16,7 +16,7 @@ final class ImmutabilityTest extends TestCase
      */
     public function testImmutability(): void
     {
-        $datetimeLocal = DateTimeLocal::create(construct: [new PropertyTypeForm(), 'string']);
+        $datetimeLocal = DatetimeLocal::create(construct: [new PropertyTypeForm(), 'string']);
         $this->assertNotSame($datetimeLocal, $datetimeLocal->max(0));
         $this->assertNotSame($datetimeLocal, $datetimeLocal->min(0));
         $this->assertNotSame($datetimeLocal, $datetimeLocal->step(0));
