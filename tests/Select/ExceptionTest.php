975ce8c90298d0c56c01b27bd0fa147d09ffb6ea
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-07-25 07:41:00 -0400

    Agregar `Select::class`. (#16)
    * Agregar Select::class.

---
+++ tests/Select/ExceptionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Forge\Form\Tests\Select;
+
+use Forge\Form\Select;
+use Forge\Form\Tests\Support\PropertyTypeForm;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use ReflectionException;
+use StdClass;
+
+final class ExceptionTest extends TestCase
+{
+    /**
+     * @throws ReflectionException
+     */
+    public function testValue(): void
+    {
+        $formModel = new PropertyTypeForm();
+
+        // Value object `stdClass`.
+        $formModel->setValue('object', new stdClass());
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Select widget value can not be an object.');
+        Select::create(construct: [$formModel, 'object'])->render();
+    }
+}
